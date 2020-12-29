dd9c0eee9c5665dbc46eec74b8956c46c7f8422e
tuan-syaripah-nur-izzati <197255@student.upm.edu.my>
2020-12-29 21:55:03 +0800

    amplify

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:324f9932-4484-4c80-8825-c4dfc10cb9c1",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_WM1kk7RMl",
+                        "AppClientId": "4cr9ghkr6m4312ih556m61frgo",
+                        "AppClientSecret": "1rts136bkpup3atmp6a92febpiadaoouh3r41fruoa76k0vji1fn",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
